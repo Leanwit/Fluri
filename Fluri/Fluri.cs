360343245faea14b855fc178c5fd3a855c3a616f
leanwit <leanwitzke@gmail.com>
2019-05-12 11:17:45 -0300

    Remove unused libraries

--- Fluri/Fluri.cs
+++ Fluri/Fluri.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Net;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
-
-namespace Fluri
+﻿namespace Fluri
 {
-    using System.Collections.Generic;
-    using System.Linq;
+    using System;
 
     public class Fluri
     {
