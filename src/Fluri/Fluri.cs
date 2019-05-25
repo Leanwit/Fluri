798955ada56b4d181072da6f7ab2c8b70e3b6be9
leanwit <leanwitzke@gmail.com>
2019-05-25 19:10:30 -0300

    Added logic for create query params with expando object

--- src/Fluri/Fluri.cs
+++ src/Fluri/Fluri.cs
@@ -1,96 +1,143 @@
 ﻿namespace Fluri
 {
     using System;
+    using System.Collections.Specialized;
+    using System.Dynamic;
+    using System.Text;
+    using System.Web;
 
     public class Fluri
     {
         private UriBuilder _uriObject;
-        
+        private NameValueCollection _queryParameters;
+
         public Fluri(string url)
         {
             this._uriObject = new UriBuilder(url);
-            
+            this._queryParameters = new NameValueCollection();
+
             //Prevent default port
             if (this._uriObject.Uri.IsDefaultPort)
             {
                 this._uriObject.Port = -1;
             }
+
+            this._queryParameters.Add(HttpUtility.ParseQueryString(this._uriObject.Query));
         }
-        
+
         public Fluri()
         {
             this._uriObject = new UriBuilder();
             this._uriObject.Port = -1;
+            this._queryParameters = new NameValueCollection();
         }
 
         public Fluri Add(string query)
         {
-            if (string.IsNullOrWhiteSpace(this._uriObject.Query))
-            {
-                this._uriObject.Query = query;
-                return this;
-            }
-            
-            this._uriObject.Query = $"{this._uriObject.Query}&{query}";
+            this._queryParameters.Add(HttpUtility.ParseQueryString(query));
+            return this;
+        }
+
+        public Fluri Over(string query)
+        {
+            this._queryParameters = new NameValueCollection();
+            this._queryParameters.Add(HttpUtility.ParseQueryString(query));
+            return this;
+        }
+
+        public Fluri Scheme(string scheme)
+        {
+            this._uriObject.Scheme = scheme;
+            return this;
+        }
+
+        public Fluri Host(string host)
+        {
+            this._uriObject.Host = host;
             return this;
         }
         
-        public Fluri Over(string query)
+        public Fluri Port(int port)
+        {
+            this._uriObject.Port = port;
+            return this;
+        }
+
+        public Fluri Fragment(string fragment)
+        {
+            this._uriObject.Fragment = fragment.Replace(" ", "%20");
+            return this;
+        }
+
+        public Fluri Fragment(int fragment)
+        {
+            this._uriObject.Fragment = fragment.ToString();
+            return this;
+        }
+
+        public Fluri Path(string path)
+        {
+            this._uriObject.Path = path;
+            return this;
+        }
+
+        public Fluri Query(string query)
         {
             this._uriObject.Query = query;
-            return this;
-        }
-
-        public Fluri Scheme(string value)
-        {
-            this._uriObject.Scheme = value;
-            return this;
-        }
-
-        public Fluri Host(string value)
-        {
-            this._uriObject.Host = value;
-            return this;
-        }
-
-        public Uri GetUri()
-        {
-            return new Uri(this.GetUrl());
-        }
-
-        public Fluri Port(int value)
-        {
-            this._uriObject.Port = value;
-            return this;
-        }
-
-        public Fluri Fragment(string value)
-        {
-            this._uriObject.Fragment = value.Replace(" ", "%20");
-            return this;
-        }
-
-        public Fluri Fragment(int value)
-        {
-            this._uriObject.Fragment = value.ToString();
-            return this;
-        }
-
-        public Fluri Path(string value)
-        {
-            this._uriObject.Path = value;
-            return this;
-        }
-
-        public Fluri Query(string value)
-        {
-            this._uriObject.Query = value;
+            this._queryParameters = new NameValueCollection();
+            this._queryParameters.Add(HttpUtility.ParseQueryString(this._uriObject.Query));
             return this;
         }
 
         public string GetUrl()
         {
+            this._uriObject.Query = String.Join("&", this.ToQueryString(this._queryParameters));
             return this._uriObject.ToString();
+        }
+        
+        public Uri GetUri()
+        {
+            this._uriObject.Query = String.Join("&", this.ToQueryString(this._queryParameters));
+            return this._uriObject.Uri;
+        }
+
+        public Fluri AddQuery(ExpandoObject query)
+        {
+            foreach (var keyValuePair in query)
+            {
+                this._queryParameters.Add(keyValuePair.Key,keyValuePair.Value.ToString());   
+            }
+
+            return this;
+        }
+        
+        public Fluri RemoveQuery(string key)
+        {
+            this._queryParameters.Remove(key);
+            return this;
+        }
+
+        private string ToQueryString(NameValueCollection nvc)
+        {
+            if (nvc == null) return string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string key in nvc.Keys)
+            {
+                if (string.IsNullOrWhiteSpace(key)) continue;
+
+                string[] values = nvc.GetValues(key);
+                if (values == null) continue;
+
+                foreach (string value in values)
+                {
+                    sb.Append(sb.Length == 0 ? "?" : "&");
+                    sb.AppendFormat("{0}={1}", key, value.Replace(" ", "+"));
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
--- test/Fluri/TestFluri.cs
+++ test/Fluri/TestFluri.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Dynamic;
+
 namespace Test
 {
     using Xunit;
@@ -59,7 +62,7 @@ namespace Test
         {
             var valueOne = "a=1&b=2";
             var valueTwo = "a=3";
-            Assert.Equal("http://google/?a=1&b=2&a=3", new Fluri("http://google/").Add(valueOne).Add(valueTwo).GetUrl());
+            Assert.Equal("http://google/?a=1&a=3&b=2", new Fluri("http://google/").Add(valueOne).Add(valueTwo).GetUrl());
         }
         
         [Fact]
@@ -67,6 +70,58 @@ namespace Test
         {
           
             Assert.Equal("https://localhost/", new Fluri().Scheme("https").GetUrl());
+        }
+        
+        [Fact]
+        public void TestQueryAddParamsByExpandoObject()
+        {
+            dynamic query = new ExpandoObject();
+            query.q = "folder";
+            query.text = "hi";
+            query.id = 356;
+            
+            Assert.Equal("http://google/?q=folder&text=hi&id=356", new Fluri("http://google/").AddQuery(query).GetUrl());
+        }
+        
+        [Fact]
+        public void TestQueryAddParamsByTwoExpandoObject()
+        {
+            dynamic queryOne = new ExpandoObject();
+            queryOne.q = "folder1";
+            queryOne.text = "hi1";
+            queryOne.id = 3561;
+            
+            dynamic queryTwo = new ExpandoObject();
+            queryTwo.q = "folder2";
+            queryTwo.text = "hi2";
+            queryTwo.id = 3562;
+
+            Assert.Equal("http://google/?q=folder1&q=folder2&text=hi1&text=hi2&id=3561&id=3562", new Fluri("http://google/").AddQuery(queryOne).AddQuery(queryTwo).GetUrl());
+        }
+        
+        [Fact]
+        public void TestRemoveKeyString()
+        {
+            dynamic query = new ExpandoObject();
+            query.q = "testing";
+            query.folder = "naming";
+
+            Assert.Equal("http://google/?q=testing", new Fluri("http://google/").AddQuery(query).RemoveQuery("folder").GetUrl());
+        }
+        
+        [Fact]
+        public void TestGetUri()
+        {
+            dynamic query = new ExpandoObject();
+            query.q = "testing";
+            query.folder = "naming";
+
+            Uri uri = new Fluri("http://google/").AddQuery(query).GetUri();
+            Assert.NotNull(uri);
+            Assert.Equal("http",uri.Scheme);
+            Assert.Equal("google",uri.Host);
+            Assert.Equal("?q=testing&folder=naming",uri.Query);
+
         }
     }
 }
